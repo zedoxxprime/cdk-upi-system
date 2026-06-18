576bd7d8ec33eb5e510b774d196efc8cee7485a0
zedoxxprime <zedoxxprime@gmail.com>
2026-06-18 21:26:19 +0500

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,15 @@
+// Firebase Admin SDK initialization for server
+const admin = require('firebase-admin');
+
+const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
+
+if (!admin.apps.length) {
+  admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+    databaseURL: process.env.FIREBASE_DATABASE_URL
+  });
+}
+
+const db = admin.firestore();
+
+module.exports = { admin, db };
